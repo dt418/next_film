082f30645cd7e0320ea4b60c9a2c7ef051699fa9
thanhd <danhthanh418@gmail.com>
2024-04-14 09:07:22 +0700

    update: update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,14 +5,13 @@ import config from "@/lib/config";
 import "dotenv/config";
 
 export default {
-
   schema: "./src/db/schema/*.ts",
   out: "./drizzle",
   driver: "mysql2", // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
   dbCredentials: {
     host: config.DB_HOST,
-    user: config.DB_USER!,
-    password: config.DB_PASSWORD!,
-    database: config.DB_NAME!,
+    user: config.DB_USER,
+    password: config.DB_PASSWORD,
+    database: config.DB_NAME,
   },
 } satisfies Config;
